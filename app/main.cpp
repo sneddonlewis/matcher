dbe5dd813a4d130a28048c791b75570e45636b9d
Lewis Sneddon <lewis.sneddon@icloud.com>
2023-09-23 15:22:35 +0100

    starter

---
+++ app/main.cpp
@@ -0,0 +1,31 @@
+// Executables must have the following defined if the library contains
+// doctest definitions. For builds with this disabled, e.g. code shipped to
+// users, this can be left out.
+#ifdef ENABLE_DOCTEST_IN_LIBRARY
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest/doctest.h"
+#endif
+
+#include "exampleConfig.h"
+
+void printVersionInfo();
+
+int main() {
+
+    printVersionInfo();
+
+    return 0;
+}
+
+void printVersionInfo() {
+    std::cout << "Hangman"
+        << PROJECT_VERSION_MAJOR
+        << "."
+        << PROJECT_VERSION_MINOR
+        << "."
+        << PROJECT_VERSION_PATCH
+        << "."
+        << PROJECT_VERSION_TWEAK
+        << std::endl;
+    std::system("cat ../LICENSE");
+}
